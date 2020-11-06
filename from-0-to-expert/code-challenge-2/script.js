af37d57f56947bad6aab87aae3c120876731ebcb
jprebelo <joaopaulo.rebelo@saphety.com>
2020-11-06 18:40:19 +0000

    code done

---
+++ from-0-to-expert/code-challenge-2/script.js
@@ -0,0 +1,16 @@
+const Mark_BMI = 28.3
+const John_BMI = 23.9
+
+// test 1
+// if (Mark_BMI > John_BMI) {
+//     console.log("Mark´s BMI is higher than John´s!")  
+// } else {
+//     console.log("John´s BMI is higher than Mark´s BMI!")
+// }
+
+// test 2
+if (Mark_BMI > John_BMI) {
+    console.log(`Mark´s BMI (${Mark_BMI}) is higher than John´s (${John_BMI})!`)  
+} else {
+    console.log(`John´s BMI (${John_BMI}) is higher than Mark´s (${Mark_BMI})!`)
+}
