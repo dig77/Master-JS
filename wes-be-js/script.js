afb2a4167bb9a25a772694ca588845f89c06bfb8
jprebelo <joaopaulo.rebelo@saphety.com>
2020-12-13 18:44:16 +0000

    wes course files

---
+++ wes-be-js/script.js
@@ -0,0 +1,56 @@
+/*
+const paragraph = document.querySelector('p')
+const paragraphs = document.querySelectorAll('p')
+const items = document.querySelectorAll('.items')
+
+const image = document.querySelectorAll('img')
+
+console.log(paragraph)
+
+paragraphs[3].classList.add('last')
+
+console.log(paragraphs)
+
+console.log(image)
+
+console.log(items)
+*/
+
+/*
+const p = document.querySelector('p')
+
+console.log(p)
+
+p.insertAdjacentHTML('beforebegin', '<h1></h1>')
+
+const h1 = document.querySelector('h1')
+
+h1.innerText = "I'm the main header for this page"
+
+const h2 = document.querySelector('h2')
+
+h2.insertAdjacentText( 'beforeend' , ' with added new text')
+
+const paragraphs = document.querySelectorAll('p')
+
+paragraphs[3].insertAdjacentText('beforeend', ' i can change this text')
+
+paragraphs[3].insertAdjacentHTML('afterend', '<strong>A new element here manipulated by JS</strong>')
+
+const images = document.querySelectorAll('img')
+
+console.log(images)
+
+const blockImg = images[1]
+
+blockImg.classList.add('is-block')
+
+function toggleRound() {
+    console.log('esta a clicar')
+    blockImg.classList.toggle('is-round');
+}
+
+console.log(blockImg)
+
+blockImg.addEventListener('click', toggleRound)
+*/
