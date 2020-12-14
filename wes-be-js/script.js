1c2292f35d929befb691d52180ec796c99dcb5c6
jprebelo <joaopaulo.rebelo@saphety.com>
2020-12-14 07:41:27 +0000

    exercises cont.

--- wes-be-js/script.js
+++ wes-be-js/script.js
@@ -1,3 +1,4 @@
+// selecting elements in DOM
 /*
 const paragraph = document.querySelector('p')
 const paragraphs = document.querySelectorAll('p')
@@ -16,6 +17,7 @@ console.log(image)
 console.log(items)
 */
 
+// working with css classes
 /*
 const p = document.querySelector('p')
 
@@ -54,3 +56,57 @@ console.log(blockImg)
 
 blockImg.addEventListener('click', toggleRound)
 */
+
+// creating HTML
+
+const myWrapper = document.createElement('div')
+
+myWrapper.classList.add('wrapper')
+
+// document.body.appendChild(myWrapper)
+
+document.body.insertAdjacentElement('afterbegin', myWrapper)
+
+const myUl = document.createElement('ul')
+
+myWrapper.appendChild(myUl)
+
+const li3 = document.createElement('li')
+
+li3.textContent = 'item 3'
+
+myUl.appendChild(li3)
+
+const li2 = document.createElement('li')
+
+li2.textContent = 'item 2'
+
+li3.insertAdjacentElement('beforebegin', li2)
+
+myUl.insertAdjacentHTML('afterbegin', '<li>item 1</li>')
+
+const li4 = document.createElement('li')
+
+li4.textContent = 'item 4'
+
+li3.insertAdjacentElement('afterend', li4)
+
+const li5 = document.createElement('li')
+
+myUl.insertAdjacentElement('beforeend', li5)
+
+li5.textContent = 'item 5'
+
+const preDiv = document.createElement('div')
+
+preDiv.classList.add('first')
+
+console.log(preDiv)
+
+myWrapper.insertAdjacentElement('beforebegin', preDiv)
+
+const h1 = document.createElement('h1')
+
+h1.textContent = 'this is main header'
+
+preDiv.appendChild(h1)
